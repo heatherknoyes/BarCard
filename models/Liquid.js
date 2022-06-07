1e871c025841e9f218d5573ebb69c9f331ee5d45
Andrew Bungcayao <thedevandrew@gmail.com>
2022-06-06 21:38:23 -0400

    Change timestamps value to 'true'

--- models/Liquid.js
+++ models/Liquid.js
@@ -1,34 +1,32 @@
-const { Model, DataTypes } = require('sequelize');
-const sequelize = require('../config/connection');
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
 
 class Liquid extends Model {}
 
-Liquid.init(
-  {
-    id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true,
-      autoIncrement: true,
-    },
-      name: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      serving_size_oz: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-      },
-      is_alcohol: {
-        type: DataTypes.BOOLEAN,
-        allowNull: false,
-      },
-      sequelize,
-      timestamps: false,
-      freezeTableName: true,
-      underscored: true,
-      modelName: 'liquid',
-  }
-);
+Liquid.init({
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  serving_size_oz: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  is_alcohol: {
+    type: DataTypes.BOOLEAN,
+    allowNull: false,
+  },
+  sequelize,
+  timestamps: true,
+  freezeTableName: true,
+  underscored: true,
+  modelName: "liquid",
+});
 
 module.exports = Liquid;
