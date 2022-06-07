6425b798a0d4e3dd196b7b8bae1d86e56937ec62
Heather Noyes <heatherknoyes@gmail.com>
2022-06-06 21:41:26 -0400

    Created node package, updated connection file, created drink model/seeds/updated index for model with starter code.

---
+++ models/index.js
@@ -0,0 +1,28 @@
+const Drink = require("./Drink");
+
+// User.hasMany(Post, {
+//   foreignKey: 'userId',
+//   onDelete: 'CASCADE',
+// });
+
+// User.hasMany(Comment, {
+//   foreignKey: 'userId',
+//   onDelete: 'CASCADE',
+// });
+
+// Post.belongsTo(User, {
+//   foreignKey: 'userId',
+//   onDelete: 'CASCADE',
+// });
+
+// Post.hasMany(Comment, {
+//   foreignKey: 'postId',
+//   onDelete: 'CASCADE',
+// });
+
+// Comment.belongsTo(User, {
+//   foreignKey: 'userId',
+//   onDelete: 'CASCADE',
+// });
+
+module.exports = { Drink };
