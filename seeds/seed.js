47cda71a04d67fba5d677e71121b3aab5232f7b3
Heather Noyes <heatherknoyes@gmail.com>
2022-06-07 20:15:47 -0400

    Update seed and model

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,9 +1,10 @@
 const sequelize = require("../config/connection");
-const { User, Drink, Review } = require("../models");
+const { User, Drink, Review, Liquid } = require("../models");
 
 const drinkData = require("./drinkData.json");
 const userData = require("./userData.json");
 const reviewData = require("./reviewData.json");
+const liquidData = require("./liquidData.json");
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
@@ -27,6 +28,12 @@ const seedDatabase = async () => {
       ...review,
       user_id: users[Math.floor(Math.random() * users.length)].id,
       drink_id: drinks[Math.floor(Math.random() * drinks.length)].dataValues.id,
+    });
+  }
+
+  for (const liquid of liquidData) {
+    await Liquid.create({
+      ...liquid,
     });
   }
 
