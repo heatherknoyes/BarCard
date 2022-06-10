7a1fbdcfe5c97fe22a1388fd87cb045400df08fa
Heather Noyes <heatherknoyes@gmail.com>
2022-06-09 23:10:53 -0400

    44 update aside to be in nav bar (#47)
    * Continuing to break apart dashboard screen
    
    * Broke up the dashboard into individual linked handlebars templates for future implementation.
    
    * Update for all routes to not use dashboard
    
    * remove dashboard.handlebars

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,14 +1,41 @@
 const router = require("express").Router();
-// This is the 'get' route
-// router.get("/frontpage", async (req, res) => {
-//   // Here, frontpage.html is rendered
-//   res.sendFile(path.join(__dirname, "../public/html/frontpage.html"));
-// });
 
 router.get("/", (req, res) => {
   try {
+    // res.render("frontpage", { logged_in: req.session.logged_in });
     res.render("frontpage");
-    // res.sendFile(path.join(__dirname, "../public/html/mainpage.html"));
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get("/account", (req, res) => {
+  try {
+    res.render("account");
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get("/newrecipe", (req, res) => {
+  try {
+    res.render("newrecipe");
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get("/newingredient", (req, res) => {
+  try {
+    res.render("newingredient");
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get("/search", (req, res) => {
+  try {
+    res.render("search");
   } catch (err) {
     res.status(500).json(err);
   }
@@ -21,19 +48,5 @@ router.get("/login", (req, res) => {
     res.status(500).json(err);
   }
 });
-
-router.get("/dashboard", (req, res) => {
-  try {
-    res.render("dashboard");
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
-
-// This is the 'get' route
-// router.get("/mainpage", async (req, res) => {
-//   // Here, mainpage.html is rendered
-//   res.sendFile(path.join(__dirname, "../public/html/mainpage.html"));
-// });
 
 module.exports = router;
