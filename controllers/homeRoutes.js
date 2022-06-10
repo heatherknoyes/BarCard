60f919a362ceb4a83a966157c3bae8abab4f9841
Heather Noyes <heatherknoyes@gmail.com>
2022-06-09 22:28:55 -0400

    Update for all routes to not use dashboard

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,9 +1,4 @@
 const router = require("express").Router();
-// This is the 'get' route
-// router.get("/frontpage", async (req, res) => {
-//   // Here, frontpage.html is rendered
-//   res.sendFile(path.join(__dirname, "../public/html/frontpage.html"));
-// });
 
 router.get("/", (req, res) => {
   try {
@@ -53,19 +48,5 @@ router.get("/login", (req, res) => {
     res.status(500).json(err);
   }
 });
-
-router.get("/dashboard", (req, res) => {
-  try {
-    res.render("dashboard");
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
-
-// This is the 'get' route
-// router.get("/mainpage", async (req, res) => {
-//   // Here, mainpage.html is rendered
-//   res.sendFile(path.join(__dirname, "../public/html/mainpage.html"));
-// });
 
 module.exports = router;
