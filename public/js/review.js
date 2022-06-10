006005089324a89d0c1907172609673e5fa7aad9
Malik Henry <mhenry30658@gmail.com>
2022-06-09 21:11:12 -0400

    initialized object-specific JS files

---
+++ public/js/review.js
@@ -0,0 +1,71 @@
+var submitButtonEl = $('.submit') // allows user to submit a comment
+var userCommentEl = $('.comment') // 
+var starEl = $('.star') // font awesome clickable stars
+var reviewEl = $('.review') // element to append reviews to the body of the review page
+var nthComment = 1; // increment for each new comment
+var starRating = 0; // star ratings out of 5
+
+
+// Rating and Review Functions
+
+function showComment() {
+   var reviews = JSON.parse(window.localStorage.getItem('reviews')) || []
+   reviews.forEach(function (comment) {
+      reviewEl.append(`<div class="box">`+ comment.userText + `</div>`)
+      for (var x=0; x<comment.userStar; x++) {
+         reviewEl.append(`<div class="fa fa-star checked"></div>`); 
+       }
+   });
+}
+
+showComment();
+ 
+function makeReview(userText, userStar) {
+   for(var i = 0; i <nthComment; i++){
+       reviewEl.append(`<div class="box">`+ userText + `</div>`);     
+   }
+}
+
+$('.fa').on('click', function () {
+   var newstarRating = $(this).attr('id'); 
+   starRating = newstarRating;
+   $(this).addClass('checked');
+
+   var array = [];
+   $(this).siblings().each( function (id, el) {
+      array.push(el.id)
+      console.log(newstarRating);
+      
+
+      if(newstarRating>el.id){
+         console.log(el.id);
+         $(el).addClass('checked');
+
+      } else if (newstarRating<el.id) {
+         $(el).removeClass('checked');
+      }
+   })
+
+   console.log(array);
+
+   return starRating;
+})
+ 
+$(".submit").on("click", function() {
+   var userText = $("#comment").val();
+   var userStar = starRating; 
+   
+   if(starRating !=0 && userText !="") {
+  var reviews = JSON.parse(window.localStorage.getItem('reviews')) || []
+  var newRating = {
+    userStar: userStar,
+    userText: userText
+  }
+ 
+  reviews.push(newRating)
+  window.localStorage.setItem("reviews", JSON.stringify(reviews));
+  makeReview(userText, userStar);
+   } else {
+      window.alert('One of the fields you have entered is blank. You must submit a comment and leave a star rating.');   
+   }
+})
